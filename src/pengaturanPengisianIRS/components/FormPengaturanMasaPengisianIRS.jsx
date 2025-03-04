98a89fa1b507314d2be370fac25b70841fec7941
monicaoktviona <monicaoktaviona2002@gmail.com>
2025-03-05 01:50:24 +0700

    modify: pengaturanPengisianIRS

--- src/pengaturanPengisianIRS/components/FormPengaturanMasaPengisianIRS.jsx
+++ src/pengaturanPengisianIRS/components/FormPengaturanMasaPengisianIRS.jsx
@@ -17,99 +17,102 @@ import {
   RichTextField,
   VisualizationAttr,
   Spinner,
-  
 } from "@/commons/components";
 import {
   ALLOWED_PERMISSIONS,
   findAllowedPermission,
 } from "@/commons/constants/allowedPermission";
 import cleanFormData from "@/commons/utils/cleanFormData";
-import updateRencanaStudi from '../services/updateRencanaStudi'
+import updateRencanaStudi from "../services/updateRencanaStudi";
 
 import { notifyError } from "@/commons/utils/toaster";
 import * as Layouts from "@/commons/layouts";
 
-const FormPengaturanMasaPengisianIRS = ({ 
-	periodePengisianIRS
- }) => {
-  const { 
-    control, 
-    handleSubmit,
-  } = useForm()
-  
-  
-  
-  
-  
-  
-  
-  const navigate = useNavigate()
-  
-  const simpan = (data) => {
-    const cleanData = cleanFormData(data)
-    updateRencanaStudi({
-      ...cleanData,
-    })
-    .then(({ data: { data } }) => {
-    })
-    .catch((error) => {
-      console.error(error);
-      notifyError(error);
-    });
-  }
-  
-  
-  return (
-	  <Layouts.FormComponentLayout
-		  title="Pengaturan Masa Pengisian IRS" 
-		  onSubmit={handleSubmit(simpan)}
-	
-	    vas={[
-		  ]}
-	
-		  formFields={[
-			  
-			  <Controller
-			    key="mulai"
-		        name="mulai"
-		        control={control}
-		        render={({ field, fieldState }) => (
-				  <InputField
-		            label="Tanggal Mulai"
-		            placeholder="Masukkan tanggal mulai"
-					type="date"
-		            fieldState={fieldState}
-					{...field}
-					isRequired={false}
-		          />
-		        )}
-		      />
-	,
-			  
-			  <Controller
-			    key="akhir"
-		        name="akhir"
-		        control={control}
-		        render={({ field, fieldState }) => (
-				  <InputField
-		            label="Tanggal Akhir"
-		            placeholder="Masukkan tanggal akhir"
-					type="date"
-		            fieldState={fieldState}
-					{...field}
-					isRequired={false}
-		          />
-		        )}
-		      />
-		  ,
-	
-		  ]}
-	
-		  itemsEvents={[
-				<Button key="Simpan" type="submit" variant="primary">Simpan</Button>
-	    ]}
-	  />
-  )
-}
+const FormPengaturanMasaPengisianIRS = ({ periodePengisianIRS }) => {
+  const { control, handleSubmit } = useForm();
 
-export default FormPengaturanMasaPengisianIRS
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const simpan = (data) => {
+    const cleanData = cleanFormData({
+      mulai: new Date(data.mulai).toISOString(),
+      akhir: new Date(data.akhir).toISOString(),
+    });
+
+    setIsLoading(true);
+    updateRencanaStudi(cleanData)
+      .then(({ data: { data } }) => {})
+      .catch((error) => {
+        console.error(error);
+        notifyError(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <>
+      {isLoading ? (
+        <div className="flex justify-center items-center h-full">
+          <Spinner />
+        </div>
+      ) : (
+        <Layouts.FormComponentLayout
+          title="Pengaturan Masa Pengisian IRS"
+          onSubmit={handleSubmit(simpan)}
+          vas={[]}
+          formFields={[
+            <Controller
+              key="mulai"
+              name="mulai"
+			  defaultValue={
+                periodeData?.mulai === undefined
+                  ? ""
+                  : new Date(periodeData?.mulai).toISOString().slice(0, -1)
+              }
+              control={control}
+              render={({ field }) => (
+                <InputField
+                  label="Tanggal Mulai"
+                  placeholder="Pilih tanggal mulai pengisian IRS"
+                  type="datetime-local"
+                  {...field}
+                />
+              )}
+			  rules={{ required: true }}
+            />,
+
+            <Controller
+              key="akhir"
+              name="akhir"
+			  defaultValue={
+                periodeData?.akhir === undefined
+                  ? ""
+                  : new Date(periodeData?.akhir).toISOString().slice(0, -1)
+              }
+              control={control}
+              render={({ field }) => (
+                <InputField
+                  label="Tanggal Akhir"
+                  placeholder="Pilih tanggal akhir pengisian IRS"
+                  type="datetime-local"
+                  {...field}
+                />
+              )}
+			  rules={{ required: true }}
+            />,
+          ]}
+          itemsEvents={[
+            <Button key="Simpan" type="submit" variant="primary">
+              Simpan
+            </Button>,
+          ]}
+        />
+      )}
+    </>
+  );
+};
+
+export default FormPengaturanMasaPengisianIRS;
