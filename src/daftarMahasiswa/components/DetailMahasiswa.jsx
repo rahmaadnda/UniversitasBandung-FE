7fc36a9c472a957d0052133169cf048aefd14b59
Rahma Adinda Putri <putrirahmaadinda@gmail.com>
2025-03-04 14:27:33 +0700

    feat: daftar mahasiswa

--- src/daftarMahasiswa/components/DetailMahasiswa.jsx
+++ src/daftarMahasiswa/components/DetailMahasiswa.jsx
@@ -23,6 +23,27 @@ const DetailMahasiswa = ({ data }) => {
       item={data}
       itemsAttrs={[
         {
+          id: "nama",
+          condition: "",
+          label: "Nama",
+          featureName: "nama",
+        }
+        ,
+        {
+          id: "nPM",
+          condition: "",
+          label: "NPM",
+          featureName: "npm",
+        }
+        ,
+        {
+          id: "email",
+          condition: "",
+          label: "Email",
+          featureName: "email",
+        }
+        ,
+        {
           id: "statusAkademik",
           condition: "",
           label: "Status Akademik",
